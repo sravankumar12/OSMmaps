d9a2573a5e0315e907db0695f4cb5874149aebd2
Nitin Surya <nitin.surya@zomato.com>
2015-04-30 11:36:04 +0000

    Generate tiles script update

--- generate_map_tile_coord.py
+++ generate_map_tile_coord.py
@@ -11,14 +11,17 @@ f = open('render_map_gen_file.sh', 'w')
 
 areas_coordinates = (
 	(28.6200, 77.3200), #delhi
-	(12.9667, 77.5667) #banglore
+	(12.9667, 77.5667), #banglore
+	(18.5203, 73.8567), #pune
+#	(51.54129068713908, -0.09257912635803223), #london
+	(19.0906, 72.9090) #mumbai
 )
 
 workers = 4
 for coordinate in areas_coordinates:
-  for zoom in range(7, 18):
+  for zoom in range(7, 19):
     result = deg2num(coordinate[0], coordinate[1], zoom)
-    inc_dec_var = 2 + 4*(zoom - 6)
+    inc_dec_var = 2 + 5*(zoom - 6)
     f.write("render_list -a -n " + str(workers) + " -x " + str(result[0] - inc_dec_var) + " -X " + str(result[0] + inc_dec_var) + " -y " + str(result[1] - inc_dec_var) + " -Y " + str(result[1] + inc_dec_var) + " -z " + str(zoom) + " -Z " + str(zoom) + "\n")
     f.write("render_list -a -n " + str(workers) + " -x " + str(result[0] - inc_dec_var) + " -X " + str(result[0] + inc_dec_var) + " -y " + str(result[1] - inc_dec_var) + " -Y " + str(result[1] + inc_dec_var) + " -z " + str(zoom) + " -Z " + str(zoom) + " -m 'style_retina'\n")
 
--- render_map_gen_file.sh
+++ render_map_gen_file.sh
@@ -1,20 +1,96 @@
-render_list -a -n 5 -x 85 -X 97 -y 47 -Y 59 -z 7 -Z 7
-render_list -a -n 5 -x 172 -X 192 -y 96 -Y 116 -z 8 -Z 8
-render_list -a -n 5 -x 351 -X 379 -y 199 -Y 227 -z 9 -Z 9
-render_list -a -n 5 -x 713 -X 749 -y 408 -Y 444 -z 10 -Z 10
-render_list -a -n 5 -x 1441 -X 1485 -y 831 -Y 875 -z 11 -Z 11
-render_list -a -n 5 -x 2901 -X 2953 -y 1681 -Y 1733 -z 12 -Z 12
-render_list -a -n 5 -x 5825 -X 5885 -y 3385 -Y 3445 -z 13 -Z 13
-render_list -a -n 5 -x 11676 -X 11744 -y 6797 -Y 6865 -z 14 -Z 14
-render_list -a -n 5 -x 23383 -X 23459 -y 13625 -Y 13701 -z 15 -Z 15
-render_list -a -n 5 -x 46801 -X 46885 -y 27284 -Y 27368 -z 16 -Z 16
-render_list -a -n 5 -x 85 -X 97 -y 47 -Y 59 -z 7 -Z 7
-render_list -a -n 5 -x 172 -X 192 -y 96 -Y 116 -z 8 -Z 8
-render_list -a -n 5 -x 351 -X 379 -y 199 -Y 227 -z 9 -Z 9
-render_list -a -n 5 -x 713 -X 749 -y 408 -Y 444 -z 10 -Z 10
-render_list -a -n 5 -x 1441 -X 1485 -y 831 -Y 875 -z 11 -Z 11
-render_list -a -n 5 -x 2901 -X 2953 -y 1681 -Y 1733 -z 12 -Z 12
-render_list -a -n 5 -x 5825 -X 5885 -y 3385 -Y 3445 -z 13 -Z 13
-render_list -a -n 5 -x 11676 -X 11744 -y 6797 -Y 6865 -z 14 -Z 14
-render_list -a -n 5 -x 23383 -X 23459 -y 13625 -Y 13701 -z 15 -Z 15
-render_list -a -n 5 -x 46801 -X 46885 -y 27284 -Y 27368 -z 16 -Z 16
+render_list -a -n 4 -x 84 -X 98 -y 46 -Y 60 -z 7 -Z 7
+render_list -a -n 4 -x 84 -X 98 -y 46 -Y 60 -z 7 -Z 7 -m 'style_retina'
+render_list -a -n 4 -x 170 -X 194 -y 94 -Y 118 -z 8 -Z 8
+render_list -a -n 4 -x 170 -X 194 -y 94 -Y 118 -z 8 -Z 8 -m 'style_retina'
+render_list -a -n 4 -x 348 -X 382 -y 196 -Y 230 -z 9 -Z 9
+render_list -a -n 4 -x 348 -X 382 -y 196 -Y 230 -z 9 -Z 9 -m 'style_retina'
+render_list -a -n 4 -x 709 -X 753 -y 404 -Y 448 -z 10 -Z 10
+render_list -a -n 4 -x 709 -X 753 -y 404 -Y 448 -z 10 -Z 10 -m 'style_retina'
+render_list -a -n 4 -x 1436 -X 1490 -y 826 -Y 880 -z 11 -Z 11
+render_list -a -n 4 -x 1436 -X 1490 -y 826 -Y 880 -z 11 -Z 11 -m 'style_retina'
+render_list -a -n 4 -x 2895 -X 2959 -y 1675 -Y 1739 -z 12 -Z 12
+render_list -a -n 4 -x 2895 -X 2959 -y 1675 -Y 1739 -z 12 -Z 12 -m 'style_retina'
+render_list -a -n 4 -x 5818 -X 5892 -y 3378 -Y 3452 -z 13 -Z 13
+render_list -a -n 4 -x 5818 -X 5892 -y 3378 -Y 3452 -z 13 -Z 13 -m 'style_retina'
+render_list -a -n 4 -x 11668 -X 11752 -y 6789 -Y 6873 -z 14 -Z 14
+render_list -a -n 4 -x 11668 -X 11752 -y 6789 -Y 6873 -z 14 -Z 14 -m 'style_retina'
+render_list -a -n 4 -x 23374 -X 23468 -y 13616 -Y 13710 -z 15 -Z 15
+render_list -a -n 4 -x 23374 -X 23468 -y 13616 -Y 13710 -z 15 -Z 15 -m 'style_retina'
+render_list -a -n 4 -x 46791 -X 46895 -y 27274 -Y 27378 -z 16 -Z 16
+render_list -a -n 4 -x 46791 -X 46895 -y 27274 -Y 27378 -z 16 -Z 16 -m 'style_retina'
+render_list -a -n 4 -x 93630 -X 93744 -y 54596 -Y 54710 -z 17 -Z 17
+render_list -a -n 4 -x 93630 -X 93744 -y 54596 -Y 54710 -z 17 -Z 17 -m 'style_retina'
+render_list -a -n 4 -x 187312 -X 187436 -y 109244 -Y 109368 -z 18 -Z 18
+render_list -a -n 4 -x 187312 -X 187436 -y 109244 -Y 109368 -z 18 -Z 18 -m 'style_retina'
+render_list -a -n 4 -x 84 -X 98 -y 52 -Y 66 -z 7 -Z 7
+render_list -a -n 4 -x 84 -X 98 -y 52 -Y 66 -z 7 -Z 7 -m 'style_retina'
+render_list -a -n 4 -x 171 -X 195 -y 106 -Y 130 -z 8 -Z 8
+render_list -a -n 4 -x 171 -X 195 -y 106 -Y 130 -z 8 -Z 8 -m 'style_retina'
+render_list -a -n 4 -x 349 -X 383 -y 220 -Y 254 -z 9 -Z 9
+render_list -a -n 4 -x 349 -X 383 -y 220 -Y 254 -z 9 -Z 9 -m 'style_retina'
+render_list -a -n 4 -x 710 -X 754 -y 452 -Y 496 -z 10 -Z 10
+render_list -a -n 4 -x 710 -X 754 -y 452 -Y 496 -z 10 -Z 10 -m 'style_retina'
+render_list -a -n 4 -x 1438 -X 1492 -y 922 -Y 976 -z 11 -Z 11
+render_list -a -n 4 -x 1438 -X 1492 -y 922 -Y 976 -z 11 -Z 11 -m 'style_retina'
+render_list -a -n 4 -x 2898 -X 2962 -y 1867 -Y 1931 -z 12 -Z 12
+render_list -a -n 4 -x 2898 -X 2962 -y 1867 -Y 1931 -z 12 -Z 12 -m 'style_retina'
+render_list -a -n 4 -x 5824 -X 5898 -y 3761 -Y 3835 -z 13 -Z 13
+render_list -a -n 4 -x 5824 -X 5898 -y 3761 -Y 3835 -z 13 -Z 13 -m 'style_retina'
+render_list -a -n 4 -x 11680 -X 11764 -y 7554 -Y 7638 -z 14 -Z 14
+render_list -a -n 4 -x 11680 -X 11764 -y 7554 -Y 7638 -z 14 -Z 14 -m 'style_retina'
+render_list -a -n 4 -x 23397 -X 23491 -y 15146 -Y 15240 -z 15 -Z 15
+render_list -a -n 4 -x 23397 -X 23491 -y 15146 -Y 15240 -z 15 -Z 15 -m 'style_retina'
+render_list -a -n 4 -x 46836 -X 46940 -y 30335 -Y 30439 -z 16 -Z 16
+render_list -a -n 4 -x 46836 -X 46940 -y 30335 -Y 30439 -z 16 -Z 16 -m 'style_retina'
+render_list -a -n 4 -x 93720 -X 93834 -y 60717 -Y 60831 -z 17 -Z 17
+render_list -a -n 4 -x 93720 -X 93834 -y 60717 -Y 60831 -z 17 -Z 17 -m 'style_retina'
+render_list -a -n 4 -x 187492 -X 187616 -y 121486 -Y 121610 -z 18 -Z 18
+render_list -a -n 4 -x 187492 -X 187616 -y 121486 -Y 121610 -z 18 -Z 18 -m 'style_retina'
+render_list -a -n 4 -x 83 -X 97 -y 50 -Y 64 -z 7 -Z 7
+render_list -a -n 4 -x 83 -X 97 -y 50 -Y 64 -z 7 -Z 7 -m 'style_retina'
+render_list -a -n 4 -x 168 -X 192 -y 102 -Y 126 -z 8 -Z 8
+render_list -a -n 4 -x 168 -X 192 -y 102 -Y 126 -z 8 -Z 8 -m 'style_retina'
+render_list -a -n 4 -x 344 -X 378 -y 212 -Y 246 -z 9 -Z 9
+render_list -a -n 4 -x 344 -X 378 -y 212 -Y 246 -z 9 -Z 9 -m 'style_retina'
+render_list -a -n 4 -x 700 -X 744 -y 436 -Y 480 -z 10 -Z 10
+render_list -a -n 4 -x 700 -X 744 -y 436 -Y 480 -z 10 -Z 10 -m 'style_retina'
+render_list -a -n 4 -x 1417 -X 1471 -y 889 -Y 943 -z 11 -Z 11
+render_list -a -n 4 -x 1417 -X 1471 -y 889 -Y 943 -z 11 -Z 11 -m 'style_retina'
+render_list -a -n 4 -x 2856 -X 2920 -y 1801 -Y 1865 -z 12 -Z 12
+render_list -a -n 4 -x 2856 -X 2920 -y 1801 -Y 1865 -z 12 -Z 12 -m 'style_retina'
+render_list -a -n 4 -x 5739 -X 5813 -y 3630 -Y 3704 -z 13 -Z 13
+render_list -a -n 4 -x 5739 -X 5813 -y 3630 -Y 3704 -z 13 -Z 13 -m 'style_retina'
+render_list -a -n 4 -x 11511 -X 11595 -y 7292 -Y 7376 -z 14 -Z 14
+render_list -a -n 4 -x 11511 -X 11595 -y 7292 -Y 7376 -z 14 -Z 14 -m 'style_retina'
+render_list -a -n 4 -x 23059 -X 23153 -y 14621 -Y 14715 -z 15 -Z 15
+render_list -a -n 4 -x 23059 -X 23153 -y 14621 -Y 14715 -z 15 -Z 15 -m 'style_retina'
+render_list -a -n 4 -x 46161 -X 46265 -y 29284 -Y 29388 -z 16 -Z 16
+render_list -a -n 4 -x 46161 -X 46265 -y 29284 -Y 29388 -z 16 -Z 16 -m 'style_retina'
+render_list -a -n 4 -x 92369 -X 92483 -y 58615 -Y 58729 -z 17 -Z 17
+render_list -a -n 4 -x 92369 -X 92483 -y 58615 -Y 58729 -z 17 -Z 17 -m 'style_retina'
+render_list -a -n 4 -x 184790 -X 184914 -y 117282 -Y 117406 -z 18 -Z 18
+render_list -a -n 4 -x 184790 -X 184914 -y 117282 -Y 117406 -z 18 -Z 18 -m 'style_retina'
+render_list -a -n 4 -x 82 -X 96 -y 50 -Y 64 -z 7 -Z 7
+render_list -a -n 4 -x 82 -X 96 -y 50 -Y 64 -z 7 -Z 7 -m 'style_retina'
+render_list -a -n 4 -x 167 -X 191 -y 102 -Y 126 -z 8 -Z 8
+render_list -a -n 4 -x 167 -X 191 -y 102 -Y 126 -z 8 -Z 8 -m 'style_retina'
+render_list -a -n 4 -x 342 -X 376 -y 211 -Y 245 -z 9 -Z 9
+render_list -a -n 4 -x 342 -X 376 -y 211 -Y 245 -z 9 -Z 9 -m 'style_retina'
+render_list -a -n 4 -x 697 -X 741 -y 434 -Y 478 -z 10 -Z 10
+render_list -a -n 4 -x 697 -X 741 -y 434 -Y 478 -z 10 -Z 10 -m 'style_retina'
+render_list -a -n 4 -x 1411 -X 1465 -y 886 -Y 940 -z 11 -Z 11
+render_list -a -n 4 -x 1411 -X 1465 -y 886 -Y 940 -z 11 -Z 11 -m 'style_retina'
+render_list -a -n 4 -x 2845 -X 2909 -y 1794 -Y 1858 -z 12 -Z 12
+render_list -a -n 4 -x 2845 -X 2909 -y 1794 -Y 1858 -z 12 -Z 12 -m 'style_retina'
+render_list -a -n 4 -x 5718 -X 5792 -y 3616 -Y 3690 -z 13 -Z 13
+render_list -a -n 4 -x 5718 -X 5792 -y 3616 -Y 3690 -z 13 -Z 13 -m 'style_retina'
+render_list -a -n 4 -x 11468 -X 11552 -y 7264 -Y 7348 -z 14 -Z 14
+render_list -a -n 4 -x 11468 -X 11552 -y 7264 -Y 7348 -z 14 -Z 14 -m 'style_retina'
+render_list -a -n 4 -x 22973 -X 23067 -y 14566 -Y 14660 -z 15 -Z 15
+render_list -a -n 4 -x 22973 -X 23067 -y 14566 -Y 14660 -z 15 -Z 15 -m 'style_retina'
+render_list -a -n 4 -x 45988 -X 46092 -y 29174 -Y 29278 -z 16 -Z 16
+render_list -a -n 4 -x 45988 -X 46092 -y 29174 -Y 29278 -z 16 -Z 16 -m 'style_retina'
+render_list -a -n 4 -x 92024 -X 92138 -y 58396 -Y 58510 -z 17 -Z 17
+render_list -a -n 4 -x 92024 -X 92138 -y 58396 -Y 58510 -z 17 -Z 17 -m 'style_retina'
+render_list -a -n 4 -x 184100 -X 184224 -y 116844 -Y 116968 -z 18 -Z 18
+render_list -a -n 4 -x 184100 -X 184224 -y 116844 -Y 116968 -z 18 -Z 18 -m 'style_retina'
